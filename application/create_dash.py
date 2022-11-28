e1b98a0307d1fcb1b03c97fde4cf0c2dde9b9499
MinhLong2410-02 <minhlong2002@gmail.com>
2022-11-28 20:14:59 +0700

    update_dash

--- application/create_dash.py
+++ application/create_dash.py
@@ -44,7 +44,7 @@ def init_dashboard(flask_app):
         html.Div(dcc.Tabs(id="tabs-styled-with-inline", value=1, children=[
             dcc.Tab(label='Dash 1', value=1, style=tab_style, selected_style=tab_selected_style),
             dcc.Tab(label='Dash 2', value=2, style=tab_style, selected_style=tab_selected_style)
-        ], style=tabs_styles), id = 'tab'),
+        ], style=tabs_styles), id = 'tab', style={'margin-top': 20}),
         html.Div(id="tabs-content-inline")
     ])
     @app.callback(Output('tabs-content-inline', 'children'),
@@ -54,14 +54,14 @@ def init_dashboard(flask_app):
             return html.Div([
         dbc.Row([
             html.Div(
-                dcc.RadioItems(id="val", style={'margin-top':20, 'color':'white'},
+                dcc.RadioItems(id="val", 
                     options = [{"label": "Nhỏ nhất", "value": "min"},
                                 {"label": "Lớn nhất", "value": "max"},
                                 {"label": "Tổng", "value": "sum"},
                                 {"label": "Trung bình", "value": "mean"},
                                 {"label": "Trung vị", "value": "median"}],
                                 value="sum"),
-            id = 'radioItems')
+            id = 'radioItems', style={'margin-top': 20, 'color': 'white'})
         ]),
         dbc.Row([
             dbc.Col([
@@ -103,7 +103,7 @@ def init_dashboard(flask_app):
                                 {"label": "Tổng", "value": "sum"},
                                 {"label": "Trung bình", "value": "mean"},
                                 {"label": "Trung vị", "value": "median"}],
-                    value="sum")],id = 'radioItems'),
+                    value="sum")],id = 'radioItems', style={'margin-top': 20, 'color':'white'}),
             dbc.Col([
                 dcc.Dropdown(id="year", 
                 options=[{"label": "2017", "value": 2017},
@@ -113,16 +113,42 @@ def init_dashboard(flask_app):
                             {"label": "2021", "value": 2021},
                             {"label": "2022", "value": 2022}],
                 multi=False,
-                value=2021)], id = 'radioItems')
+                value=2021)], id = 'radioItems', style={'margin-top': 20})
         ]),
         dbc.Row([
             dbc.Col([
                 dbc.Card([
-                    html.P("Tốc độ gió trung bình", className='title'),
-                    dcc.Graph(id="tmax_month")
+                    html.P("Biểu đồ nhiệt độ", className='title'),
+                    dcc.Graph(id="temp_month")
                 ])
-            ], width=12)
-        ], style={'margin-top': 20})
+            ], width=6, style={'margin-top': 20}),
+            dbc.Col([
+                dbc.Card([
+                    html.P("Biểu đồ lượng mưa", className='title'),
+                    dcc.Graph(id="prcp_month")
+                ])
+            ], width=6, style={'margin-top': 20})
+        ], className='row1'),
+        dbc.Row([
+            dbc.Col([
+                dbc.Card([
+                    html.P("Biểu đồ hộp nhiệt độ", className='title'),
+                    dcc.Graph(id="temp_box")
+                ])
+            ], width=4, style={'margin-top': 20}),
+            dbc.Col([
+                dbc.Card([
+                    html.P("Biểu đồ tốc độ gió", className='title'),
+                    dcc.Graph(id="wind_month")
+                ])
+            ], width=4, style={'margin-top': 20}),
+            dbc.Col([
+                dbc.Card([
+                    html.P("Biểu đồ hộp lượng mưa", className='title'),
+                    dcc.Graph(id="prcp_box")
+                ])
+            ], width=4, style={'margin-top': 20})
+        ], className='row2')
     ])
 
     @app.callback([Output("prcp_year", "figure"),
@@ -211,4 +237,134 @@ def init_dashboard(flask_app):
         )
         fig4.update_layout(yaxis_title=None, xaxis_title=None, margin={"r": 5, "t": 5, "l": 5, "b": 5})
         return fig1, fig2, fig3, fig4
+
+    @app.callback([Output("temp_month", "figure"),
+                Output("prcp_month", "figure"),
+                Output("temp_box", "figure"),
+                Output("wind_month", "figure"),
+                Output("prcp_box", "figure")],
+                [(Input("val", "value")),
+                (Input("year", "value"))])
+    def display_graph(val, year):
+        yearly = df[df.index.year == year]
+        monthly = None
+        if val == 'sum':
+            monthly = yearly.resample('M').sum()
+        elif val == 'min': 
+            monthly = yearly.resample('M').min()
+        elif val == 'max':
+            monthly = yearly.resample('M').max()
+        elif val == 'mean': 
+            monthly = yearly.resample('M').mean()
+        else: 
+            monthly = yearly.resample('M').median()
+        
+        # temp
+        y_m = monthly.tmax.max()
+        x_m = monthly.loc[monthly.tmax == y_m].index.month[0]
+        fig1 = go.Figure()
+        fig1.add_trace(go.Scatter(x=monthly.index.month, y=monthly['tmin'],
+            name=ele['tmin'],
+            connectgaps=True)
+        )
+
+        fig1.add_trace(go.Scatter(x=monthly.index.month, y=monthly['tmax'],
+            name=ele['tmax'],
+            connectgaps=True,)
+        )
+        fig1.update_layout(
+            barmode='overlay',
+            title_font_color="red",
+            font_color="yellow",
+            hovermode="x unified",
+            hoverlabel=dict(
+                bgcolor="gray",
+                font_size=16,
+                font_family="Rockwell"),
+            showlegend = False
+        )
+        fig1.add_annotation(
+                x=x_m,
+                y=y_m,
+                xref="x",
+                yref="y",
+                text=f"max: {y_m}",
+                showarrow=True,
+                font=dict(
+                    family="Courier New, monospace",
+                    size=16,
+                    color="#ffffff"
+                    ),
+                align="center",
+                arrowhead=2,
+                arrowsize=1,
+                arrowwidth=2,
+                arrowcolor="#00e600",
+                ax=20,
+                ay=-30,
+                bordercolor="#c7c7c7",
+                borderwidth=2,
+                borderpad=4,
+                bgcolor="#ff7f0e",
+                opacity=0.8
+                )
+        fig1.update_layout(yaxis_title=None, xaxis_title=None, margin=dict(l=5, r=5, t=5, b=5), height=300)
+        
+        # prcp
+        fig2 = px.bar(x = monthly.index.month, y=monthly['prcp'],
+                    text_auto=True,
+                    labels={
+                        "x": "",
+                        "y": ""
+                        })
+        fig2.update_layout(
+            barmode='overlay',
+            title_font_color="red",
+            font_color="yellow",
+            showlegend = False,
+            plot_bgcolor='black'
+        )
+        fig2.update_layout(yaxis_title=None, xaxis_title=None, margin=dict(l=5, r=5, t=5, b=5), height=300)
+
+        #box
+        fig3 = go.Figure()
+        fig3.add_trace(go.Box(
+            y=monthly.tmax,
+            name='',
+            jitter=0.3,
+            pointpos=-1.8,
+            boxpoints='all',
+            marker_color='#66ff33',
+            line_color='#ff66cc'
+        ))
+        fig3.update_layout(yaxis_title=None, xaxis_title=None, margin=dict(l=5, r=5, t=5, b=5))
+        #awnd
+        fig4 = px.line_polar(monthly, r = 'awnd', theta=list(map(lambda x: str(x), monthly.index.month.values)),
+                    labels={
+                        "awnd": "Tốc độ gió",
+                        "theta": "Tháng"},
+                    line_close=True,
+                    
+                    )
+        fig4.update_layout(
+            barmode='overlay',
+            font_color="yellow",
+            showlegend = False
+        )
+        fig4.update_traces(fill='toself')
+        fig4.update_traces(mode="markers+lines")
+
+        # box prcp
+        fig5 = go.Figure()
+        fig5.add_trace(go.Box(
+            y=monthly.prcp,
+            name='',
+            jitter=0.3,
+            pointpos=-1.8,
+            boxpoints='all',
+            marker_color='#66ff33',
+            line_color='#ff66cc'
+        ))
+        fig5.update_layout(yaxis_title=None, xaxis_title=None, margin=dict(l=5, r=5, t=5, b=5))
+        return fig1, fig2, fig3, fig4, fig5
     return app
